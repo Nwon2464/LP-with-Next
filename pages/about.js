eff374af95498ae384e1ac8e386938eef523e2db
dnjsskarb7 <64752079+dnjsskarb7@users.noreply.github.com>
2020-12-22 23:50:56 +0900

    not found

--- pages/about.js
+++ pages/about.js
@@ -1,6 +1,7 @@
 import { useEffect } from "react";
 import { useRouter } from "next/router";
 import Layout from "../components/Layout";
+//Not found !
 function About(props) {
   useEffect(() => {}, []);
   const { pathname } = useRouter();
